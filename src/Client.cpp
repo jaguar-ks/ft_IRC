e606487715b39b9920558563d7973bd8875d016e
jaguar-ks <fahdamineksouss@gmail.com>
2024-02-14 20:28:25 +0100

    PROGRESS : Athentication is ready and sends the replys it should now will move to the next commands

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,113 +2,122 @@
 
 // Default Constructor
 Client::Client(int ClntFd, in_addr *ClntAddr) : ClntFd(ClntFd), Regestred(false) {
-    this->Athentication["PASS"] = static_cast<bool (Client::*)(string)>(&Client::setSrvPss);
-    this->Athentication["NICK"] = static_cast<bool (Client::*)(string)>(&Client::setNckName);
-    this->Athentication["USER"] = static_cast<bool (Client::*)(string)>(&Client::setUsrName);
-    // this->Athentication.insert(pair<string, void (Client::*)(string &)>(string("PASS"), static_cast<void (Client::*)(string &)>(&Client::setSrvPss)));
-    // this->Athentication.insert(pair<string, void (Client::*)(string &)>(string("NICK"), static_cast<void (Client::*)(string &)>(&Client::setNckName)));
-    // this->Athentication.insert(pair<string, void (Client::*)(string &)>(string("USER"), static_cast<void (Client::*)(string &)>(&Client::setUsrName)));
+    this->Athentication["PASS"] = static_cast<bool (Client::*)(vector<string>)>(&Client::setSrvPss);
+    this->Athentication["NICK"] = static_cast<bool (Client::*)(vector<string>)>(&Client::setNckName);
+    this->Athentication["USER"] = static_cast<bool (Client::*)(vector<string>)>(&Client::setUsrName);
     this->HstName = inet_ntoa(*ClntAddr);
 }
 
 void    Client::setCmd(string line) {
-    size_t i = 0;
-    while (line.find(' ') != string::npos && !line.empty()) {
-        this->Cmd.push_back(line.substr(0, line.find(' ')));
-        i = line.find(' ');
-        for (; i < line.size() && line.at(i) == ' '; i++);
-        if (line.empty() || line.at(i) == ':') {
-            i+=(!line.empty());
+    string tmp;
+    size_t x = 0;
+    this->Cmd.push_back(string());
+    for (size_t i = 0; i < line.size(); i++){
+        if (line.at(i) == ':') {
+            this->Cmd[x] = line.substr(i+1);
             break ;
         }
-        line = line.substr(i);
+        if (line.at(i) != ' ')
+            this->Cmd[x] += line.at(i);
+        else {
+            for (; i+1 < line.size() && line.at(i+1) == ' '; i++);
+            this->Cmd.push_back(string());
+            x++;
+        }
     }
-    if (!line.empty())
-        this->Cmd.push_back(line.substr(i));
+    // if (this->Cmd.back().empty())
+    //     this->Cmd.pop_back();
 }
 
 bool    Client::ParsAndExec() {
-    // (this->Msg.find('\n') != string::npos) ? this->Cmd.push_back(this->Msg.substr(0, this->Msg.find(' '))) : this->Cmd.push_back(this->Msg);
     this->setCmd(this->Msg);
     for (size_t i = 0; i < this->Cmd.size(); i++)
         cout << this->Cmd[i] << ((i + 1 != this->Cmd.size()) ? "|" : "|\n");
     if (!this->Regestred) {
-        if (this->Athentication.find(this->Msg.substr(0,4)) != this->Athentication.end())
-            (this->*Athentication[this->Msg.substr(0,4)])(this->Msg);
+        if (this->Athentication.find(this->Cmd[0]) != this->Athentication.end())
+            (this->*Athentication[this->Cmd[0]])(this->Cmd);
         else
             cerr << "Invalid Command" << endl;
-        // this->Regestred = true;
-        // cout << Server::getInstance()->getPswd() << endl;
-        // if (this->Athentication.find(this->Msg.substr(0,4)) != this->Athentication.end())
-            // (this->*Athentication[this->Msg.substr(0,4)])(this->Msg);
-        // else
-        //     cout << "No such Command or Command not authorised" << endl;
+        this->Regestred = (!this->SrvPss.empty() && !this->NckName.empty() && !this->UsrName.empty());
+        cout << this->SrvPss << " | " << this->NckName << " | " << this->UsrName << endl;
     }
-    else
-    {
+    else {
         cout << "Do Command" << endl;
-
     }
     this->Msg = "";
     this->Cmd.clear();
     return true;
 }
 
-bool    Client::setNckName(string nick)
+bool    Client::setNckName(vector<string> cmd)
 {
-    nick.erase(0, 5);
-    nick.erase(0, nick.find_first_not_of(" \f\n\r\t\v"));
-    nick.erase(nick.find_last_not_of(" \f\n\r\t\v")+1);
-    for (size_t i = 0; i < nick.size(); i++)
-    {
-        if (!i && isdigit(nick[i]))
-            return (false) ;
-        if (!isalnum(nick[i]) && nick[i] != '{' && nick[i] != '}' && nick[i] != '[' && nick[i] != ']' && nick[i] != '\\' && nick[i] != '|' &&  nick[i] != '_')
-            return (false) ;
-    }
-    for (map<int, Client>::iterator it = Server::getInstance()->getClients().begin(); it != Server::getInstance()->getClients().end(); it++)
-    {
-        if (it->second.getNckName() == nick)
-            return (false) ;
-    }
-    this->NckName = nick;
-    return (true);
-}
-
-bool		  Client::setUsrName(string user)
-{
-    stringstream params(user);
-
-    params >> user;
-    if(!(params >> user))
-        return (false);
-    this->UsrName = user;
-    if (!(params >> user))
-        return (false);
-    if (!(params >> user))
-        return (false);
-    if (!getline(params, user))
-        return (false);
-    user.erase(0, user.find_first_not_of(" \f\n\r\t\v"));
-    if (user[0] == ':')
-    {
-        user.erase(0, 1);
-        this->RlName = user;
+    string msg;
+    if (cmd.size() == 2) {
+        if (isalpha(cmd[1].at(0)) || cmd[1].at(0) == '_') {
+            string allowed = "[]{}|\\_";
+            size_t i = 1;
+            for (; i < cmd[1].size(); i++)
+                if (!isalnum(cmd[1].at(i)) && allowed.find(cmd[1].at(i)) == string::npos)
+                    break ;
+            if (i != cmd[1].size())
+                msg = ":ircserv 432 " + this->NckName + " :Erroneus nickname\r\n";
+            else {
+                map<int, Client> Clnts = Server::getInstance()->getClients();
+                map<int, Client>::iterator it = Clnts.begin();
+                for (; it != Clnts.end(); it++)
+                    if (it->second.NckName == cmd[1])
+                        break ;
+                if (it != Clnts.end())
+                    msg = ":ircserv 433 " + this->NckName + " :Nickname is already in use\r\n";
+                else {
+                    this->NckName = cmd[1];
+                    return true;
+                }
+            }
+        }
+        else
+            msg = ":ircserv 432 " + this->NckName + " :Erroneus nickname\r\n";
     }
     else
-    {
-        if (user.find_first_of(" \f\n\r\t\v") != string::npos)
-            return (false);
-        this->RlName = user;
-    }
-    return (true);
-}
-
-bool		  Client::setSrvPss(string password)
-{
-    password.erase(0, 5);
-    password.erase(0, password.find_first_not_of(" \f\n\r\t\v"));
-    if (Server::getInstance()->getPswd() == password)
-        return (true);
+        msg = ":ircserv 431 " + this->NckName + " :No nickname given\r\n";
+    send(this->ClntFd, msg.c_str(), msg.size(), 0);
     return (false);
 }
+
+bool		  Client::setUsrName(vector<string> cmd)
+{
+    string msg;
+    if (cmd.size() == 5){
+        if (!this->Regestred){
+            this->UsrName = cmd[1];
+            this->RlName = cmd[4];
+            return true;
+        }
+        else
+            msg = ":ircserv 462 " + this->NckName + " :You may not reregister\r\n";
+    }
+    else
+        msg = ":ircserv 461 " + this->NckName + " " + cmd[0] + " :Not enough parameters\r\n";
+    send(this->ClntFd, msg.c_str(), msg.size(), 0);
+    return (false);
+}
+
+bool		  Client::setSrvPss(vector<string> cmd)
+{
+    string msg;
+    if (cmd.size() == 2) {
+        if (this->Regestred)
+            msg = ":ircserv 462 " + this->NckName + " :You may not reregister\r\n";
+        else if(Server::getInstance()->getPswd() == cmd[1]) {
+            this->SrvPss = cmd[1];
+            return true;
+        }
+        else
+            msg = ":ircserv 464 " + this->NckName + " :Password incorrect\r\n";
+    }
+    else
+        msg = ":ircserv 461 " + this->NckName + " " + cmd[0] + " :Not enough parameters\r\n";
+    send(this->ClntFd, msg.c_str(), msg.size(), 0);
+    return (false);
+}
+// REPLYFORMAT [":<ServerName> <StatusCode> <CLientNick> :<Msg>"]
