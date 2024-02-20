542fa617a12a5312a5a9e825665b50c3af138573
bleuuone <bleuuone@gmail.com>
2024-02-19 22:27:52 -0500

    ADD BOT

--- bot/BtcPrice.cpp
+++ bot/BtcPrice.cpp
@@ -46,4 +46,46 @@ void		BtcPrice::connectToServer()
 	// std::cout << ":<servername> "<< this->getName() << " has joined the server" << std::endl;
 }
 
+std::string extractCmd(std::string msg)
+{
+	std::string		cmd;
+	cmd = msg.substr(0, msg.find(" "));
+	return cmd;
+}
+
+std::string			extractNick(std::string msg)
+{
+	std::string		nick;
+	nick = msg.substr(1, msg.find("!") - 1);
+	return nick;
+}
+
+void			BtcPrice::botReply(std::string msg)
+{
+	std::string	cmd, nick, Msg, botName;
+	std::stringstream	ss(msg);
+	// std::cout << nick << std::endl;
+	if (msg.size() <= 2)
+		return;
+	msg.erase(msg.size() - 2);
+	ss >> nick >> cmd >> botName >> Msg;
+	nick = extractNick(msg);
+	std::string finalMsg = static_cast<std::string>("PRIVMSG") + " " + nick + " " + "42666.56" + "\r\n";
+	std::cout << finalMsg << std::endl;
+	std::cout << cmd << std::endl;
+	if (cmd == "BTCPRICE")
+		send(this->getSocketFd(), finalMsg.c_str(), finalMsg.size(), 0);
+	// std::cout << "nick: " << nick << std::endl;
+	// std::cout << "cmd: " << cmd << std::endl;
+	// std::cout << "msg: " << Msg << std::endl;
+	// std::cout << "botName: " << botName << std::endl;
+	// std::string		cmd;
+	std::cout << msg << std::flush;
+	// if (msg.size() > 0)
+	// {
+	// 	msg.erase(0, msg.find_first_of(" \t\n\v\f\r") + 1);
+
+	// }
+}
+
 BtcPrice::~BtcPrice() {}
