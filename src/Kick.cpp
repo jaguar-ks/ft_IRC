1d09731046ee8d18a25c84fee146ea8ab8285fca
jaguar-ks <fahdamineksouss@gmail.com>
2024-02-25 14:13:46 +0100

    FIX : the Password policy and empty password

--- src/Kick.cpp
+++ src/Kick.cpp
@@ -7,69 +7,6 @@
  * @return True if the client was successfully kicked, false otherwise.
  */
 
-// bool    Client::Kick(vector<string> cmd) {
-//     Server *srv = Server::getInstance();
-//     if (cmd.size() == 4 || cmd.size() == 3)
-//     {
-//         if (srv->isChannel(cmd[1]))//srv->getChannels().find(cmd[1]) != srv->getChannels().end()) // 
-//         {
-//             Channel *chnl =srv->getChannel(cmd[1]);
-//             if (!chnl->isMember(this)) 
-//             {
-//                 if (chnl->isOperator(this)) 
-//                 {
-//                     if (srv->getClientByNckName(cmd[2]) < 0) 
-//                     {
-//                         ErrorMsgGenrator(":IRCserv.1337.ma 401 ", " " + cmd[2] + " :No such nick", *this);
-//                         return false;
-//                     }
-//                     vector<Client *>::iterator it = chnl->getMembers().begin();
-//                     for (; it != chnl->getMembers().end(); it++)
-//                     {
-//                         if ((*it)->NckName == cmd[2])
-//                             break;
-//                     }
-//                     if (it == chnl->getMembers().end()) 
-//                     {
-//                         ErrorMsgGenrator(":IRCserv.1337.ma 441 ", " " + cmd[2] + " " + cmd[1] + " :They aren't on that channel", *this);
-//                         return false;
-//                     }
-//                     if (cmd.size() == 3)
-//                         SendMsg(*this, *chnl, cmd[0], " " , cmd[1] + " " + cmd[2]);
-//                     else
-//                         SendMsg(*this, *chnl, cmd[0], cmd[3] , cmd[1] + " " + cmd[2]);
-//                     VcRemove((*it)->Chnls, cmd[1]);
-//                     chnl->kickUser(this, *it);
-//                     if (chnl->getMembers().empty()) 
-//                     {
-//                         delete chnl;
-//                         srv->getChannels().erase(cmd[1]);
-//                     }
-//                     return true;
-//                 }
-//                 else 
-//                 {
-//                     ErrorMsgGenrator(":IRCserv.1337.ma 482 ", " " + cmd[1] + " :You're not channel operator", *this);
-//                     return false;
-//                 }
-//             }
-//             else 
-//             {
-//                 ErrorMsgGenrator(":IRCserv.1337.ma 442 ", " " + cmd[1] + " :You're not on that channel", *this);
-//                 return false;
-//             }
-//         }
-//         else
-//         {
-//             ErrorMsgGenrator(":IRCserv.1337.ma 403 ", " " + cmd[1] + " :No such channel", *this);
-//             return false;
-//         }
-//     }
-//     else
-//     {
-//     }
-// }
-
 bool Client::Kick(vector<string> cmd) {
     if (cmd.size() == 3  || cmd.size() == 4) {
         Server *srv = Server::getInstance();
